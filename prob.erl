851eb56764533dc7130ad7f0dbe65d024a8c9be3
James Aimonetti <james.aimonetti@gmail.com>
2010-02-22 14:14:20 -0500

    euler-style problem

---
+++ prob.erl
@@ -0,0 +1,28 @@
+-module(prob).
+
+-export([answer/0]).
+
+%% You have the numbers 123456789, in that order. Between each number, you must
+%% insert either nothing, a plus sign, or a multiplication sign, so that the
+%% resulting expression equals 2002. Operator precedence does apply.
+
+answer() -> find(lists:seq(1,9)).
+
+find(L) -> build(L, []).
+
+build([], E) -> Ex = [$2, $0, $0, $2, $=,$:,$=] ++ lists:reverse(E) ++ [$.],
+                 case eval(Ex) of
+                     {value, true, _} ->
+                         io:format("E: ~p~n", [Ex]);
+                     _Else -> false
+                 end;
+build([H|T], []) -> build(T, [H+$0]);
+build([H|T], E) ->
+    build(T, [H+$0, $+ | E]),
+    build(T, [H+$0, $* | E]),
+    build(T, [H+$0 | E]).
+
+eval(S) ->
+    {ok,Scanned,_} = erl_scan:string(S),
+    {ok,Parsed} = erl_parse:parse_exprs(Scanned),
+    erl_eval:exprs(Parsed,[]).
