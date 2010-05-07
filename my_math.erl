f55a55519ccf3b477fa5425e7239693a683de0bc
James Aimonetti <james.aimonetti@gmail.com>
2010-05-07 15:03:30 -0400

    powerset fun

--- my_math.erl
+++ my_math.erl
@@ -3,6 +3,7 @@
 -export([gcd/1, gcd/2,
          lcm/1, lcm/2,
          perms/1, perms/2,
+         powerset/1,
          fac/1,
          fib/1,
          fib_generator/0,
@@ -40,6 +41,14 @@ perms(N, R) ->
 perms([]) -> [[]];
 perms(L) ->
     [ [H|T] || H <- L, T <- perms(L--[H]) ].
+
+%% powerset of [1,2,3] => [[], [1], [2], [3], [1,2], [1,3], [2,3], [1,2,3]]
+powerset(L) ->
+    N = length(L),
+    Max = trunc(math:pow(2,N)),
+    [[lists:nth(Pos+1,L) || Pos <- lists:seq(0,N-1),
+                            I band (1 bsl Pos) =/= 0]
+     || I <- lists:seq(0,Max-1)].
 
 fac(X) ->
     fac(X, 1).
